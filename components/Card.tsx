ff7bf9f077a0d2fe16f50e190dd0954be2d00ecd
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-07 04:19:27 +0800

    update style

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Card = ({ children, src, alt }: Props) => {
   return (
     <div className="flex flex-col w-60 rounded-2xl border-2 border-solid border-gray-200 overflow-hidden">
-      <div className="w-full h-56 mb-6 border-b-2 border-solid border-gray-200">
+      <div className="w-full h-56 border-b-2 border-solid border-gray-200">
         <img src={src} alt={alt} style={{ width: '100%', height: '100%' }} />
       </div>
       <div className="p-4">{children}</div>
