a61c2309cc023e095d9daf7cb3ad8d1427e07448
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-08 05:01:43 +0800

    commented unneeded functions

--- redux/reducers/postReducers.ts
+++ redux/reducers/postReducers.ts
@@ -6,17 +6,17 @@ const initialState = {
   loading: false,
   error: null,
 }
-export const postReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case types.GET_POSTS:
-      return {
-        ...state,
-        posts: action.payload,
-        loading: false,
-        error: null,
-      }
+// export const postReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case types.GET_POSTS:
+//       return {
+//         ...state,
+//         posts: action.payload,
+//         loading: false,
+//         error: null,
+//       }
 
-    default:
-      return state
-  }
-}
+//     default:
+//       return state
+//   }
+// }
