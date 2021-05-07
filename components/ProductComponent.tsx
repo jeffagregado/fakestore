2003506bebb66a5dbb203c65b9cd4335ca983705
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-07 19:36:31 +0800

    fixed key prop error

--- components/ProductComponent.tsx
+++ components/ProductComponent.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 //import Card from './Card'
 import dynamic from 'next/dynamic'
 import Loader from './Loader'
+import React from 'react'
 
 const ProductComponent = () => {
   const products = useSelector((state: State) => state.allproducts.products)
@@ -20,14 +21,10 @@ const ProductComponent = () => {
     <>
       {products.map((product) => {
         return (
-          <>
+          <React.Fragment key={product.id}>
             <Link href={`/products/[id]`} as={`/products/${product.id}`}>
               <a>
-                <DynamicCard
-                  key={product.id}
-                  src={product.image}
-                  alt={product.title}
-                >
+                <DynamicCard src={product.image} alt={product.title}>
                   <div className="mb-4">
                     <h1 className="font-bold mb-2">{product.title}</h1>
                     <h2 className="font-medium">${product.price}</h2>
@@ -38,7 +35,7 @@ const ProductComponent = () => {
                 </DynamicCard>
               </a>
             </Link>
-          </>
+          </React.Fragment>
         )
       })}
     </>
