76e97efaae34f8eb91beedbb17abff5e894551ec
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-08 21:20:23 +0800

    update style for loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,10 +2,12 @@ import loaderStyle from '../styles/Loader.module.scss'
 
 const Loader = () => {
   return (
-    <div className={loaderStyle["lds-facebook"]}>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="container flex justify-center items-center min-h-screen">
+      <div className={loaderStyle['lds-facebook']}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   )
 }
