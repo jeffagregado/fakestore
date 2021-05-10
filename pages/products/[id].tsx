0cfc993b45530d96c493db68e14a94a5af9354b5
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2021-05-11 06:08:30 +0800

    created static pages for individual items

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -4,36 +4,38 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import CardProduct from '../../components/CardProduct'
 import Loader from '../../components/Loader'
+import { GetStaticPaths } from 'next'
 import {
   selectedProduct,
   removeProduct,
 } from '../../redux/actions/productActions'
 
-const Products = () => {
-  const product = useSelector((state: State) => state.product)
-  const { title, category, price, description, image }: ProductType = product
-  const dispatch = useDispatch()
-  const router = useRouter()
-  const { id }: any = router.query
-  const fetchProductDetail = async () => {
-    await axios
-      .get(`https://fakestoreapi.com/products/${id}`)
-      .then((res) => {
-        dispatch(selectedProduct(res.data))
-      })
-      .catch((err) => {
-        console.log('Err', err)
-      })
-  }
-  useEffect(() => {
-    if (id && id !== '') fetchProductDetail()
-    return () => {
-      dispatch(removeProduct())
-    }
-  }, [id])
+const Products = ({ product }: any) => {
+  // const product = useSelector((state: State) => state.product)
+  // const { title, category, price, description, image }: ProductType = product
+  // const dispatch = useDispatch()
+  // const router = useRouter()
+  // const { id }: any = router.query
+  // const fetchProductDetail = async () => {
+  //   await axios
+  //     .get(`https://fakestoreapi.com/products/${id}`)
+  //     .then((res) => {
+  //       dispatch(selectedProduct(res.data))
+  //     })
+  //     .catch((err) => {
+  //       console.log('Err', err)
+  //     })
+  // }
+  // useEffect(() => {
+  //   if (id && id !== '') fetchProductDetail()
+  //   return () => {
+  //     dispatch(removeProduct())
+  //   }
+  // }, [id])
+
   return (
     <>
-      {Object.keys(product).length === 0 ? (
+      {/* {Object.keys(product).length === 0 ? (
         <Loader />
       ) : (
         <>
@@ -45,9 +47,42 @@ const Products = () => {
             image={image}
           />
         </>
-      )}
+      )} */}
+      <CardProduct
+            title={product.title}
+            category={product.category}
+            price={product.price}
+            description={product.description}
+            image={product.image}
+          />
     </>
   )
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch('https://fakestoreapi.com/products')
+  const data = await res.json()
+
+  const paths = data.map((item: any) => {
+    return {
+      params: { id: item.id.toString() },
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async (context: any) => {
+  const id = context.params.id
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`)
+  const data = await res.json()
+
+  return {
+    props: { product: data },
+  }
 }
 
 export default Products
